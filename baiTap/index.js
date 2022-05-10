29477e1224ee2526bed128a4900bdefcbddebfd6
HaHaiBinh <habinh500@gmail.com>
2022-05-10 23:36:52 +0700

    up file

---
+++ baiTap/index.js
@@ -0,0 +1,89 @@
+/* Bai 1
+Input: Tạo biến ngày, tạo biến tiền lương
+
+Các bước xử lý
+B1: tạo 2 biến số ngày và tiền lương
+B2: tạo biến lưu kết quả
+B3: công thức tính: tiền lương * số ngày
+
+Output: Kết quả (tiền)
+*/
+
+// var soNgay = 3 ;
+// const tienLuong = 100.000 ;
+
+// result = soNgay * tienLuong;
+
+// console.log("result", result);
+
+/* Bai 2
+Input: Tạo 5 biến số thực
+
+Các bước xử lý 
+B1: tạo biến số thực 
+B2: tạo biến lưu kết quả
+B3: công thức tính: tổng 5 số thực / 5 số thực
+
+Output: Kết quả
+*/
+
+// var soThuc1 = 1;
+// var soThuc2 = 2;
+// var soThuc3 = 3;
+// var soThuc4 = 4;
+// var soThuc5 = 5;
+
+// result = (soThuc1 + soThuc2 + soThuc3 + soThuc4 + soThuc5) / 5;
+
+// console.log("result",result);
+
+/* Bai3
+Input: Tạo biến tiền quy đổi USA, tạo biến số tiền USA cần quy đổi
+
+Các bước xử lý 
+B1: tạo biến tiền quy đổi, số tiền cần quy đổi
+B2: tạo biến kết quả
+B3: công thức tính: số tiền quy đổi * tiền quy đổi
+
+Output: Kết quả
+
+*/
+
+// var tienQuyDoi = 23.500;
+// var sotienUSA = 2;
+
+// result = tienQuyDoi * sotienUSA;
+// console.log("result", result);
+
+/* Bai4
+Input : Tạo 2 biến chiều dài và chiều rộng
+
+Các bước xử lý
+B1: tạo 2 biến chiều dài và chiều rộng
+B2: tạo biến kết quả
+B3: công thức tính
+- Diện tích: chiều dài * chiều rộng
+- Chu vi: (chiều dài + chiều rộng) / 2
+
+Output: Kết quả 
+*/
+
+// var chieuDai = 3; 
+// var chieuRong = 5;
+
+// resultDienTich = chieuDai * chieuRong;
+// resultChuVi = (chieuDai + chieuRong) / 2; 
+
+// console.log(resultDienTich);
+// console.log(resultChuVi);
+
+/* Bai5
+Input: 
+
+*/
+var soCanTinh = 44;
+var so_hang_dv = soCanTinh / 10;
+var so_hang_chuc = soCanTinh % 10;
+
+result = so_hang_chuc + so_hang_dv ;
+console.log("result", result);
